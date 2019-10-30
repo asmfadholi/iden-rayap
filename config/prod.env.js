4265a6e9b038c16e789e09025fdf0503b4844daa
asmfadholi <fadholi@jojonomic.com>
2019-10-30 09:52:41 +0700

    hardcode env

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,5 +6,5 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/../.env` });
 
 module.exports = {
-  NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+  NODE_ENV: "'production'",
 }
